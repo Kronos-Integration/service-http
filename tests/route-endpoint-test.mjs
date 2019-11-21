2140f6f7fda9eaa9c51d6e04631ecb1c976e3afc
Markus Felten <Markus.Felten@gmx.de>
2019-11-21 14:58:23 +0100

    test: wip

--- tests/route-endpoint-test.mjs
+++ tests/route-endpoint-test.mjs
@@ -34,10 +34,12 @@ test("endpoint route basics", async t => {
   );
   //t.truthy(r2.hasInterceptors);
   console.log(`r2: ${r2}`);
-
+  
   const s2 = new SendEndpoint("s2");
   s2.receive = async () => "OK S2";
   r2.connected = s2;
+
+  console.log(`r2: ${r2}`);
 
   ks.koa.use(endpointRouter(ks));
 
