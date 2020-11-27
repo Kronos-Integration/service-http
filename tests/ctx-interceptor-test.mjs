02e32e670a6dfb743f9f5fe179ebe1d7f9e5fab0
Markus Felten <Markus.Felten@gmx.de>
2020-11-27 21:21:06 +0100

    test: use interceptorTest

--- tests/ctx-interceptor-test.mjs
+++ tests/ctx-interceptor-test.mjs
@@ -1,13 +1,51 @@
 import test from "ava";
 import { TestContext } from "./helpers/context.mjs";
+import {
+  interceptorTest,
+  dummyEndpoint
+} from "@kronos-integration/test-interceptor";
 import { SendEndpoint } from "@kronos-integration/endpoint";
 import { StandaloneServiceProvider } from "@kronos-integration/service";
 import { ServiceHTTP, CTXInterceptor } from "@kronos-integration/service-http";
 
-test("defaults", async t => {
-  const interceptor = new CTXInterceptor();
-  t.deepEqual(interceptor.headers, {});
-});
+
+test(
+  interceptorTest,
+  CTXInterceptor,
+  undefined,
+  { json: { headers: {}, type: "ctx" } },
+  dummyEndpoint("ep1"),
+  [new TestContext()],
+  () => undefined,
+  async (t, interceptor, endpoint, next, result, ctx) => {
+    t.is(ctx.code, 200);
+  }
+);
+
+test(
+  interceptorTest,
+  CTXInterceptor,
+  {
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate"
+    }
+  },
+  {
+    json: {
+      headers: {
+        "Cache-Control": "no-store, no-cache, must-revalidate"
+      },
+      type: "ctx"
+    }
+  },
+  dummyEndpoint("ep1"),
+  [new TestContext()],
+  () => "result",
+  async (t, interceptor, endpoint, next, result, ctx) => {
+    t.is(ctx.code, 200);
+    t.is(ctx.headers["cache-control"], "no-store, no-cache, must-revalidate");  
+  }
+);
 
 test("default headers", async t => {
   const sp = new StandaloneServiceProvider();
