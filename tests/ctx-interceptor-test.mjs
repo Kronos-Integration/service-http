ea69d03f1b045c64c1e4b1b2bae7d0a9b74777f8
Markus Felten <Markus.Felten@gmx.de>
2020-11-28 21:30:44 +0100

    test: remove duplicates

--- tests/ctx-interceptor-test.mjs
+++ tests/ctx-interceptor-test.mjs
@@ -4,18 +4,15 @@ import {
   interceptorTest,
   dummyEndpoint
 } from "@kronos-integration/test-interceptor";
-import { SendEndpoint } from "@kronos-integration/endpoint";
-import { StandaloneServiceProvider } from "@kronos-integration/service";
-import { ServiceHTTP, CTXInterceptor } from "@kronos-integration/service-http";
-
+import { CTXInterceptor } from "@kronos-integration/service-http";
 
 test(
   interceptorTest,
   CTXInterceptor,
   undefined,
   { json: { headers: {}, type: "ctx" } },
-  dummyEndpoint("ep1"),
-  [new TestContext()],
+  dummyEndpoint("e1"),
+  [new TestContext({ body: "" })],
   () => undefined,
   async (t, interceptor, endpoint, next, result, ctx) => {
     t.is(ctx.code, 200);
@@ -38,36 +35,11 @@ test(
       type: "ctx"
     }
   },
-  dummyEndpoint("ep1"),
-  [new TestContext()],
+  dummyEndpoint("e1"),
+  [new TestContext({ body: "" }), "extra1"],
   () => "result",
   async (t, interceptor, endpoint, next, result, ctx) => {
     t.is(ctx.code, 200);
-    t.is(ctx.headers["cache-control"], "no-store, no-cache, must-revalidate");  
+    t.is(ctx.headers["cache-control"], "no-store, no-cache, must-revalidate");
   }
 );
-
-test("default headers", async t => {
-  const sp = new StandaloneServiceProvider();
-  const http = await sp.declareService({
-    type: ServiceHTTP
-  });
-  const endpoint = new SendEndpoint("e", http);
-
-  const configuredHeaders = {
-    "Cache-Control": "no-store, no-cache, must-revalidate"
-  };
-
-  const interceptor = new CTXInterceptor({
-    headers: configuredHeaders
-  });
-
-  t.deepEqual(interceptor.headers, configuredHeaders);
-
-  const ctx = new TestContext();
-
-  await interceptor.receive(endpoint, (ctx, a, b, c) => {}, ctx, 1, 2, 3);
-
-  t.is(ctx.code, 200);
-  t.is(ctx.headers["cache-control"], "no-store, no-cache, must-revalidate");
-});
